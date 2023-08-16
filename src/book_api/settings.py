420f5f810de1d190fdf5992e7f58640a3f2d679f
0xshodan <hngdean@gmail.com>
2023-08-16 22:23:32 +0300

    chore: docker dev finish setup

--- src/book_api/settings.py
+++ src/book_api/settings.py
@@ -1,11 +1,13 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = ""
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -53,8 +55,12 @@ WSGI_APPLICATION = "book_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "db",
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
