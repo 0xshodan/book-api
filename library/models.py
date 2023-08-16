f897f2daaf17f986cdae64c3f214cf6fcf6438af
0xshodan <hngdean@gmail.com>
2023-08-16 17:44:36 +0300

    chore: make some fields nullable

--- library/models.py
+++ library/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    date_of_birth = models.DateField()
-    biography = models.TextField()
+    date_of_birth = models.DateField(null=True, blank=True)
+    biography = models.TextField(null=True, blank=True)
 
 
 class Book(models.Model):
